32e1df25e1aba8e8d86e9cf020b63ac117bc674e
EnderLove <cocpowergameplays@gmail.com>
2025-09-21 12:59:00 -0500

    Updated setup.bat

--- setup.bat
+++ setup.bat
@@ -3,6 +3,7 @@ setlocal
 
 set VCPKG_ROOT=%~dp0vcpkg
 
+:: Clone vcpkg if missing
 if not exist "%VCPKG_ROOT%" (
     echo Cloning vcpkg...
     git clone https://github.com/microsoft/vcpkg.git "%VCPKG_ROOT%"
@@ -12,9 +13,17 @@ if not exist "%VCPKG_ROOT%" (
 if not exist build mkdir build
 cd build
 
-cmake .. -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
-
-cmake --build . --config Release
+:: Try MSVC first
+where cl >nul 2>nul
+if %ERRORLEVEL%==0 (
+    echo Found MSVC compiler (Visual Studio).
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake -G "Visual Studio 17 2022" -A x64
+    cmake --build . --config Release
+) else (
+    echo No MSVC found, falling back to MinGW/Makefiles...
+    cmake .. -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=Release
+    cmake --build .
+)
 
 echo.
 echo ====== Setup complete! ======
