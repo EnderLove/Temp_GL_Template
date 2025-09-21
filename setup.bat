27d9c85fd54f9444a7bfe891511e192cec79fbf2
EnderLove <cocpowergameplays@gmail.com>
2025-09-21 12:20:50 -0500

    Last update

--- setup.bat
+++ setup.bat
@@ -3,7 +3,7 @@ setlocal
 
 set VCPKG_ROOT=%~dp0vcpkg
 
-if not exist "%VCPKG_ROOT%"(
+if not exist "%VCPKG_ROOT%" (
     echo Cloning vcpkg...
     git clone https://github.com/microsoft/vcpkg.git "%VCPKG_ROOT%"
     call "%VCPKG_ROOT%\bootstrap-vcpkg.bat"
@@ -12,7 +12,7 @@ if not exist "%VCPKG_ROOT%"(
 if not exist build mkdir build
 cd build
 
-cmake .. -CDMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake -DCMAKE_BUILD_TYPE=Release
 
 cmake --build . --config Release
 
